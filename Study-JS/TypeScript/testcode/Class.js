3372e6803277ae036f1f4e34e73354f2e2d23af7
CodeReading <kangyongsuek@gmail.com>
2019-01-27 19:38:04 +0900

    add: typescript 스터디에 따른 테스트 코드 추가

--- Study-JS/TypeScript/testcode/Class.js
+++ Study-JS/TypeScript/testcode/Class.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Employee = /** @class */ (function () {
     function Employee(code, name) {
         this.empName = name;
@@ -16,4 +29,24 @@ var Employee2 = /** @class */ (function () {
     return Employee2;
 }());
 var empObj = new Employee(100, "CodeReading");
-console.log(empObj);
+console.log(empObj); //{ empName: 'CodeReading', empCode: 100 }
+var Person = /** @class */ (function () {
+    function Person(name) {
+        this.name = name;
+    }
+    return Person;
+}());
+var Employee3 = /** @class */ (function (_super) {
+    __extends(Employee3, _super);
+    function Employee3(empcode, name) {
+        var _this = _super.call(this, name) || this;
+        _this.empCode = empcode;
+        return _this;
+    }
+    Employee3.prototype.displayName = function () {
+        console.log("Name = " + this.name + ", Employess Code = " + this.empCode);
+    };
+    return Employee3;
+}(Person));
+var emp = new Employee3(100, "Kang");
+emp.displayName();
--- Study-JS/TypeScript/testcode/Class.ts
+++ Study-JS/TypeScript/testcode/Class.ts
@@ -25,3 +25,27 @@ class Employee2 {
 
 let empObj = new Employee(100, "CodeReading");
 console.log(empObj); //{ empName: 'CodeReading', empCode: 100 }
+
+class Person {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+class Employee3 extends Person {
+    empCode: number;
+
+    constructor(empcode: number, name: string) {
+        super(name);
+        this.empCode = empcode;
+    }
+
+    displayName():void {
+        console.log(`Name = ${this.name}, Employess Code = ${this.empCode}`)
+    }
+}
+
+let emp = new Employee3(100, "Kang");
+emp.displayName();
